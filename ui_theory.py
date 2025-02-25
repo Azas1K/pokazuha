7eefc25918fc1749cc73cdbe6f3f9b76de024ff8
Alexey-Nikolaevic-B <alexey.bardushko@gmail.com>
2025-02-25 23:16:30 +0300

    #3 animation

--- ui_theory.py
+++ ui_theory.py
@@ -26,46 +26,67 @@ class TheoryScreen(QDialog):
     T = 0.05  # Длительность сигнала в секундах
     time = np.linspace(0, T, int(fs*T), endpoint=False)  # Временная ось
 
-
+    carrier = Ac * np.sin(2*np.pi*fc*time)
 
     def __init__(self):
         super(TheoryScreen, self).__init__()
 
         self.init_ui()
 
+    def graph_1(self):
+        self.control_layer = self.widget
+        self.control_layer = QGridLayout(self.control_layer)
+    
+        figure = plt.figure()
+        ax = plt.axes(xlim=(0, 0.1), ylim=(-400, 400))
+        
+        figure.patch.set_facecolor((210/256, 210/256, 210/256))
+        ax.set_title(f'Несущий сигнал')
+        ax.set_ylabel('Амплитуда, дБ')
+        ax.set_xlabel('Время')
+
+        self.line1, = ax.plot([], [], lw=2)
+        self.anim1 = FuncAnimation(figure, self.update_plot1, frames=200, interval=20, blit=True)
+
+        self.radial_1 = FigureCanvas(figure)
+        self.control_layer.addWidget(self.radial_1,0,0)
+
     def graph_2(self):
-        self.calc()
+        self.control_layer = self.widget_2
+        self.control_layer = QGridLayout(self.control_layer)
+    
+        figure = plt.figure()
+        ax_2 = plt.axes(xlim=(0, 0.1), ylim=(-1.5, 1.5))
+        
+        figure.patch.set_facecolor((210/256, 210/256, 210/256))
+        ax_2.set_title(f'Несущий сигнал')
+        ax_2.set_ylabel('Амплитуда, дБ')
+        ax_2.set_xlabel('Время')
 
-        # self.control_layer = self.widget_2
-        # self.control_layer = QGridLayout(self.control_layer)
+        self.line2, = ax_2.plot([], [], lw=2)
+        self.anim2 = FuncAnimation(figure, self.update_plot2, frames=200, interval=20, blit=True)
 
-        # self.figure_2, self.ax = plt.subplots(figsize=(10, 5))
-        # self.figure_2.patch.set_facecolor((210/256, 210/256, 210/256))
-        # self.ax.plot(self.time, self.modulating)
-        # self.ax.set_title(f'Модулирующий сигнал')
-        # self.ax.set_ylabel('Амплитуда, дБ')
-        # self.ax.set_xlabel('Время')
-
-        # self.radial_2 = FigureCanvas(self.figure_2)
-
-        # self.control_layer.addWidget(self.radial_2,0,0)
+        self.radial_2 = FigureCanvas(figure)
+        self.control_layer.addWidget(self.radial_2,0,0)
 
     def graph_3(self):
         self.calc()
-        # self.control_layer = self.widget_3
-        # self.control_layer = QGridLayout(self.control_layer)
+        self.control_layer = self.widget_3
+        self.control_layer = QGridLayout(self.control_layer)
     
-        # self.figure_3, self.ax = plt.subplots(figsize=(10, 5))
-        # self.figure_3.patch.set_facecolor((210/256, 210/256, 210/256))
-        # self.ax.plot(self.time, self.modulated)
-        # self.canvas = FigureCanvas(self.figure_3)
-        # self.ax.set_title(f'Модулированный сигнал')
-        # self.ax.set_ylabel('Амплитуда, дБ')
-        # self.ax.set_xlabel('Время')
+        figure = plt.figure()
+        ax_3 = plt.axes(xlim=(0, 0.1), ylim=(-600, 600))
+        
+        figure.patch.set_facecolor((210/256, 210/256, 210/256))
+        ax_3.set_title(f'Несущий сигнал')
+        ax_3.set_ylabel('Амплитуда, дБ')
+        ax_3.set_xlabel('Время')
 
-        # self.radial_3 = FigureCanvas(self.figure_3)
+        self.line3, = ax_3.plot([], [], lw=2)
+        self.anim3 = FuncAnimation(figure, self.update_plot3, frames=200, interval=20, blit=True)
 
-        # self.control_layer.addWidget(self.radial_3,0,0)
+        self.radial_3 = FigureCanvas(figure)
+        self.control_layer.addWidget(self.radial_3,0,0)
 
     def graph_4(self):
         self.calc()
@@ -113,40 +134,76 @@ class TheoryScreen(QDialog):
         # self.freqs = fftfreq(self.N, 1/self.fs)  # Частоты спектра
         # self.spectrum = np.abs(fft(self.modulated)) / self.N  # Спектр
 
-    def graph_1(self):
-        self.control_layer = self.widget
-        self.control_layer = QGridLayout(self.control_layer)
-    
-        figure = plt.figure()
-        ax = plt.axes(xlim=(0, 0.5), ylim=(-400, 400))
-        
-        figure.patch.set_facecolor((210/256, 210/256, 210/256))
-        ax.set_title(f'Несущий сигнал')
-        ax.set_ylabel('Амплитуда, дБ')
-        ax.set_xlabel('Время')
 
-        self.line1, = ax.plot([], [], lw=2)
-        self.anim = FuncAnimation(figure, self.update_plot1, frames=200, interval=20, blit=True)
-
-        self.radial_1 = FigureCanvas(figure)
-        self.control_layer.addWidget(self.radial_1,0,0)
 
     def update_plot1(self, i):
-            # carrier = Ac * np.sin(2*np.pi*fc*time) # несущий сигнал
-        x = np.linspace(0, 2, 1000)
-        y = self.Ac * np.sin(2*np.pi*self.fc*(x + 0.02 * i))
+        self.fc = self.qs_carrier_freq.value() / 2
+        speed = self.qs_speed.value() / 10000
+        x = np.linspace(0, 2, 20000)
+        y = self.Ac * np.sin(2*np.pi*self.fc*(x + (0.0001 + speed )* i))
         self.line1.set_data(x, y)
         return self.line1,
 
-    def update_plot2(self, frame):
-        new_y_data = self.current_modulating_signal(self.t + 0.01 * frame, self.freq_modulator, self.harmonic_count)
-        # self.line2.set_ydata(new_y_data)
-        self.radial_2.draw_idle()
+    def update_plot2(self, i):
+        self.mod_type = self.cb_mod.currentText()
+        speed = self.qs_speed.value() / 10000
+        x = np.linspace(0, 2, 10000)
 
-    def update_plot3(self, frame):
-        new_y_data = self.current_modulated_signal(self.t + 0.01 * frame, self.freq_carrier, self.freq_modulator, self.harmonic_count)
-        # self.line3.set_ydata(new_y_data)
-        self.radial_3.draw_idle()
+        y = self.Am[0] * np.sin(2*np.pi*self.fm[0]*(x + (0.0001 + speed )* i))/80
+        # for i in range(1, self.fm_count):
+        #     y += self.Am[i] * np.sin(2*np.pi*self.fm[i]*(x + (0.0001 + speed )* i))/80
+
+        if self.mod_type == "ЧМн" or self.mod_type == "ФМн":
+            y = np.sign(np.sin(2 * np.pi * self.fm[0]*(x + (0.0001 + speed )* i)))
+
+        self.line2.set_data(x, y)
+        return self.line2,
+
+    def update_plot3(self, i):
+        self.mod_type = self.cb_mod.currentText()
+        speed = self.qs_speed.value() / 10000
+        x = np.linspace(0, 2, 20000)
+
+        carrier = self.Ac * np.sin(2*np.pi*self.fc*x)
+
+        self.modulating = self.Am[0] * np.sin(2*np.pi*self.fm[0]*(x + (0.0001 + speed )* i))
+        for i in range(1, self.fm_count):
+            self.modulating += self.Am[i] * np.sin(2*np.pi*self.fm[i]*(x + (0.0001 + speed )* i))
+
+        if self.mod_type == "АМ":
+            y = (1 + self.m * self.modulating) * carrier
+        elif self.mod_type == "ОМ":
+            y = (self.m * self.modulating) * carrier
+        elif self.mod_type == "ЧМ":
+            y= self.Ac * np.sin(2*np.pi*self.fc*(x + (0.0001 + speed )* i) + self.m*2*np.pi*self.modulating)
+        elif self.mod_type == "ЧМн":
+            self.modulating = np.sign(np.sin(2 * np.pi * self.fm[0] * (x + (0.0001 + 0 )* i)))
+            self.integral_mod = np.cumsum(self.modulating) / self.fs
+            y = self.Ac * np.sin(2 * np.pi * self.fc * (x + (0.0001 + 0 )* i) + 2000*self.m*np.pi* self.integral_mod)
+        elif self.mod_type == "ФМн":
+            self.modulating = np.sign(np.sin(2 * np.pi * self.fm[0] * (x + (0.0001 + 0 )* i)))
+            y = self.Ac * np.cos(2 * np.pi * self.fc * (x + (0.0001 + 0 )* i) + (np.pi * (self.modulating + 1) / 2))
+
+        self.line3.set_data(x, y)
+        return self.line3,
+
+
+    def update_plot4(self, i):
+        self.mod_type = self.cb_mod.currentText()
+        speed = self.qs_speed.value() / 10000
+        x = np.linspace(0, 2, 10000)
+
+        y = self.Am[0] * np.sin(2*np.pi*self.fm[0]*(x + (0.0001 + speed )* i))/80
+        # for i in range(1, self.fm_count):
+        #     y += self.Am[i] * np.sin(2*np.pi*self.fm[i]*(x + (0.0001 + speed )* i))/80
+
+        if self.mod_type == "ЧМн" or self.mod_type == "ФМн":
+            y = np.sign(np.sin(2 * np.pi * self.fm[0]*(x + (0.0001 + speed )* i)))
+
+        self.line2.set_data(x, y)
+        return self.line2,
+
+
 
     def init_ui(self):
         loadUi('qt/theory.ui', self)
@@ -155,7 +212,3 @@ class TheoryScreen(QDialog):
         self.graph_2()
         self.graph_3()
         self.graph_4()
-
-        # 
-        # self.animation2 = FuncAnimation(self.radial_1, self.update_plot2, frames=200, interval=50)
-        # self.animation3 = FuncAnimation(self.radial_1, self.update_plot3, frames=200, interval=50)
